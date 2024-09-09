e242ff7db8e0e661a777c3a1e91d448975fa432a
Keyvan Shabani <54222332+keyvan1996@users.noreply.github.com>
2024-09-09 00:56:02 +0000

    added page

--- src/pages/login.ts
+++ src/pages/login.ts
@@ -3,11 +3,13 @@ import logger from "../utils/logger";
 import * as dataLogin from '../data/login.json'
 
 export class LoginPage {
+  private page: Page
   private usernameField: Locator;
   private passwordField: Locator;
   private loginButton: Locator;
 
   constructor(page: Page) {
+    this.page = page;
     this.usernameField = page.locator('input[placeholder="Username"]');
     this.passwordField = page.locator('input[placeholder="Password"]');
     this.loginButton = page.locator("input#login-button");
